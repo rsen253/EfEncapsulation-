d574afb0535f384ae7b6693544478b07cc01fe7c
Rahul Sen <rsen253@gmail.com>
2017-08-28 16:05:02 +0530

    - Added Product data logic and controller

---
+++ MvcSalesApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,111 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using MvcSalesApp.Data;
+using MvcSalesApp.Domain;
+
+namespace MvcSalesApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ProductData repo = new ProductData();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View(repo.GetAllCustomers());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = repo.FindProduct(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductId,Description,Name,ProductionStart,IsAvailable")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.AddProduct(product);
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = repo.FindProduct(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductId,Description,Name,ProductionStart,IsAvailable")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.UpdateProduct(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = repo.FindProduct(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            repo.RemoveProduct(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
